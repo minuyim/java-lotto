4050ca573a3e6cba64c08d6a68d0ef8c6187a7fc
minuyim <minu_yim@naver.com>
2020-03-06 00:40:23 +0900

    [refactor] 기타 컨벤션 수정

--- src/main/java/domain/generator/UserNumberGenerator.java
+++ src/main/java/domain/generator/UserNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class UserNumberGenerator implements NumberGenerator {
-    public static final String ERROR_REPEAT_END_MESSAGE = "입력한 번호를 모두 반환하였습니다.";
+    private static final String ERROR_REPEAT_END_MESSAGE = "입력한 번호를 모두 반환하였습니다.";
 
     private Iterator<List<Integer>> userInputs;
 
